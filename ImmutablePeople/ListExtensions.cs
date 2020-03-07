c68caa3809623e6d929aaa142605334dffce6bee
Marek Nagy <62247071@cuni.cz>
2020-03-07 18:43:13 +0100

    [WIP] Person classes

---
+++ ImmutablePeople/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmutablePeople
+{
+    public static class ListExtensions
+    {
+        public static void PrintAll(this List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person + " has password \"" + person.Password + "\"");
+            }
+        }
+    }
+}
--- ImmutablePeople/Person.cs
+++ ImmutablePeople/Person.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ImmutablePeople
 {
-    public abstract class PersonG<T> where T:PersonG<T>
+    public abstract class PersonG<T> where T: PersonG<T>
     {
         public string FirstName {get; private set;}
         public string LastName {get; private set;}
@@ -32,22 +32,33 @@ namespace ImmutablePeople
     }
     public abstract class Person : PersonG<Person>
     {
-
     }
     public class Student : PersonG<Student>
     {
+        public DateTime DateEnrolled { get; private set;}
         public static Student Default => new Student();
         public Student WithDateEnrolled(DateTime dateEnrolled)
         {
-            
+            this.DateEnrolled = dateEnrolled;
+            return this;
+        }
+        public override string ToString()
+        {
+            return "Student " + FirstName + " " + LastName;
         }
     }
     public class Teacher : PersonG<Teacher>
     {
+        public int Coureses { get; private set;}
         public static Teacher Default => new Teacher();
         public Teacher WithCoursesHeld(int coursesAmount)
         {
-            
+            this.Coureses = coursesAmount;
+            return this;
+        }
+        public override string ToString()
+        {
+            return "Teacher " + FirstName + " " + LastName;
         }
     }
 }
