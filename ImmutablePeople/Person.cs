5673f4875cdf45f6446716e39603437e6dacd0f3
Marek Nagy <62247071@cuni.cz>
2020-03-08 13:13:41 +0100

    [WIP] still the same

--- ImmutablePeople/Person.cs
+++ ImmutablePeople/Person.cs
@@ -9,40 +9,49 @@ namespace ImmutablePeople
         public string FirstName {get; set;}
         public string LastName {get; set;}
         public string Password {get; set;}
-        //protected T WithFirstName<T>(string firstName) where T: Person
-        //{
-        //    this.FirstName = firstName;
-        //    return (T) this;
-        //}
-        //protected T WithLastName<T>(string lastName) where T: Person
-        //{
-        //    this.LastName = lastName;
-        //    return (T) this;
-        //}
-        //public T WithName<T>(string fullName) where T: Person
-        //{
-        //    string[] nameParts = fullName.Split(' ');
-        //    return this.WithFirstName<T>(nameParts[0]).WithLastName<T>(nameParts[1]);
-        //}
-        //public T WithPassword<T>(string passwd) where T: Person
-        //{
-        //    this.Password = passwd;
-        //    return (T) this;
-        //}
+        public abstract Person WithName(string fullName);
+        public abstract Person WithPassword(string passwd);
     }
-    public abstract class Person<T> : Person where T: Person<T>, new()
+    public abstract class Person<T> : Person where T: Person<T>
     {
-        
+        public abstract T WithName<T>(string fullName);
+        public abstract T WithPassword<T>(string passwd);
+        public override sealed Person WithName(string fullName)
+        {
+            return WithName<T>(fullName);
+        }
+        public override sealed Person WithPassword(string passwd)
+        {
+            return WithPassword<T>(passwd);
+        }
     }
 
     public class Student : Person<Student>
     {
+        public Student(string firstName, string lastName, string password, DateTime dateEnrolled)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Password = password;
+            this.DateEnrolled = dateEnrolled;
+        }
         public DateTime DateEnrolled { get; private set;}
-        public static Student Default => new Student();
+        public static Student Default => new Student("", "", "", DateTime.Now);
         public Student WithDateEnrolled(DateTime dateEnrolled)
         {
             this.DateEnrolled = dateEnrolled;
             return this;
+        }
+
+        public override Person WithName<T>(string fullName) 
+        {
+            string[] nameParts = fullName.Split(' ');
+            return new Student(nameParts[0], nameParts[1], this.Password, this.DateEnrolled);
+        }
+
+        public override Person WithPassword<T>(string passwd)
+        {
+            return new Student(this.FirstName, this.LastName, passwd, this.DateEnrolled);
         }
         public override string ToString()
         {
@@ -51,16 +60,33 @@ namespace ImmutablePeople
     }
     public class Teacher : Person<Teacher>
     {
-        public int Coureses { get; private set;}
-        public static Teacher Default => new Teacher();
+        public Teacher(string firstName, string lastName, string password, int courses)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Password = password;
+            this.Courses = courses;
+        }
+        public int Courses { get; private set;}
+        public static Teacher Default => new Teacher("","","",0);
         public Teacher WithCoursesHeld(int coursesAmount)
         {
-            this.Coureses = coursesAmount;
-            return this;
+            return new Teacher(this.FirstName, this.LastName, this.Password, coursesAmount);
         }
         public override string ToString()
         {
             return "Teacher " + FirstName + " " + LastName;
+        }
+
+        public override Person WithName<T>(string fullName)
+        {
+            string[] nameParts = fullName.Split(' ');
+            return new Teacher(nameParts[0], nameParts[1], this.Password, this.Courses);
+        }
+
+        public override Person WithPassword<T>(string passwd)
+        {
+            return new Teacher(this.FirstName, this.LastName, passwd, this.Courses);
         }
     }
 }
