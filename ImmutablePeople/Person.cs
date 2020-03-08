bef7e4370d59dea42ee8dab40b97fb5b472175c0
Marek Nagy <62247071@cuni.cz>
2020-03-08 14:53:13 +0100

    Added protected set

--- ImmutablePeople/Person.cs
+++ ImmutablePeople/Person.cs
@@ -14,9 +14,9 @@ namespace ImmutablePeople
         {
 
         }
-        public string FirstName {get;}
-        public string LastName {get;}
-        public string Password {get;}
+        public string FirstName {get; protected set;}
+        public string LastName {get; protected set;}
+        public string Password {get; protected set;}
         public abstract Person WithNameP(string fullName);
         public abstract Person WithPasswordP(string passwd);
     }
