2b7181619c2766bc3f86297a20feac1f75aeeed3
Marek Nagy <62247071@cuni.cz>
2020-03-10 09:48:10 +0100

    Added ListExtensionsTests

--- ImmutablePeopleTests/ListExtensionTests.cs
+++ ImmutablePeopleTests/ListExtensionTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ImmutablePeople;
 using Xunit;
 
@@ -6,20 +7,22 @@ namespace ImmutablePeopleTests
 {
     public class ListExtensionTests
     {
-        [Fact]
-        public void PrintAllTest()
-        {
-
-        }
+        //unable to test PrintAll
         [Fact]
         public void WithPasswordResetByFirstNameTest1()
         {
+            //Arrange
+            List<Person> list = new List<Person>();
+            list.Add(Student.Default.WithName("Marek Nagy").WithPassword("great_passwd") );
+            list.Add(Teacher.Default.WithName("Peter Nagy").WithPassword("good_passwd") );
 
-        }
-        [Fact]
-        public void WithPasswordResetByFirstNameTest2()
-        {
+            //Act
+            var updated = list.WithPasswordResetByFirstName(firstName: "Marek", newPassword: "super_Password");
 
+            //Assert
+            Assert.Equal("super_Password", updated[0].Password);
+            Assert.Equal("good_passwd", updated[1].Password);
+            
         }
     }
 }
